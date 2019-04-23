8ba6a2dc79eb7a47d7fe432fdcf6eade779a4bff
Kshitij Patil <kshitijpatil98@gmail.com>
2019-04-24 03:00:54 +0530

    binding child view successful

--- app/src/main/java/com/kshitijpatil/tusk/ui/listproducts/ProductListActivity.java
+++ app/src/main/java/com/kshitijpatil/tusk/ui/listproducts/ProductListActivity.java
@@ -3,16 +3,27 @@ package com.kshitijpatil.tusk.ui.listproducts;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.kshitijpatil.tusk.R;
+import com.kshitijpatil.tusk.databinding.ActivityProductListBinding;
+import com.kshitijpatil.tusk.ui.data.model.Product;
+
+import java.util.List;
 
 public class ProductListActivity extends AppCompatActivity implements ListProductContract.View {
     boolean isLoading = false;
-
+    ActivityProductListBinding binding;
+    ProductListPresenter<ListProductContract.View> presenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_list);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product_list);
+
+        presenter = new ProductListPresenter<>();
+
+        List<Product> productList = presenter.getProductList();
+        binding.setProductFirst(new Product("Amazon Echo", 7999, "Amazon Echo is a hands-free speaker you control with your voice", 4.7f));
     }
 
     @Override
--- app/src/main/java/com/kshitijpatil/tusk/ui/listproducts/ProductListPresenter.java
+++ app/src/main/java/com/kshitijpatil/tusk/ui/listproducts/ProductListPresenter.java
@@ -3,12 +3,21 @@ package com.kshitijpatil.tusk.ui.listproducts;
 import com.kshitijpatil.tusk.Injector;
 import com.kshitijpatil.tusk.ui.base.BasePresenter;
 import com.kshitijpatil.tusk.ui.data.Repository;
+import com.kshitijpatil.tusk.ui.data.model.Product;
+
+import java.util.List;
 
 public class ProductListPresenter<V extends ListProductContract.View> extends BasePresenter<V> implements ListProductContract.Presenter<V> {
     private static final String TAG = "ProductListPresenter";
-    Repository repository;
+    private Repository repository;
+
 
     public ProductListPresenter() {
         repository = Injector.provideRepository();
     }
+
+    public List<Product> getProductList() {
+        return repository.getProducts();
+    }
+
 }
