31ab6d1cc09446a94653b752def7be5bcc1efa54
Kshitij Patil <kshitijpatil98@gmail.com>
2019-04-24 20:58:32 +0530

    final commit before presentation

--- app/src/main/java/com/kshitijpatil/tusk/ui/cart/CartActivity.java
+++ app/src/main/java/com/kshitijpatil/tusk/ui/cart/CartActivity.java
@@ -3,6 +3,7 @@ package com.kshitijpatil.tusk.ui.cart;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.kshitijpatil.tusk.R;
@@ -10,12 +11,17 @@ import com.kshitijpatil.tusk.R;
 public class CartActivity extends AppCompatActivity {
     private static final String TAG = "CartActivity";
     RecyclerView recyclerCart;
-
+    CartPresenter<CartContract.View> presenter;
+    CartAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
+        presenter = new CartPresenter<>();
+        adapter = new CartAdapter(presenter.getCartItems());
         recyclerCart = findViewById(R.id.recycler_cart);
+        recyclerCart.setLayoutManager(new LinearLayoutManager(this));
+        recyclerCart.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/kshitijpatil/tusk/ui/cart/CartAdapter.java
+++ app/src/main/java/com/kshitijpatil/tusk/ui/cart/CartAdapter.java
@@ -1,4 +1,54 @@
 package com.kshitijpatil.tusk.ui.cart;
 
-public class CartAdapter {
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kshitijpatil.tusk.R;
+import com.kshitijpatil.tusk.data.model.Product;
+import com.kshitijpatil.tusk.databinding.ItemCartListBinding;
+
+import java.util.List;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartItemHolder> {
+    List<Product> cartList;
+    private LayoutInflater inflater;
+
+
+    CartAdapter(List<Product> cartList) {
+        this.cartList = cartList;
+    }
+
+
+    @NonNull
+    @Override
+    public CartItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (inflater == null)
+            inflater = LayoutInflater.from(parent.getContext());
+        ItemCartListBinding binding = DataBindingUtil.inflate(inflater, R.layout.item_cart_list, parent, false);
+
+        return new CartItemHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartItemHolder holder, int position) {
+        holder.binding.setProduct(cartList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return cartList.size();
+    }
+
+    class CartItemHolder extends RecyclerView.ViewHolder {
+        ItemCartListBinding binding;
+
+        public CartItemHolder(ItemCartListBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
 }
